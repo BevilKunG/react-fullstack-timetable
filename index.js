b7362d493236c90c0d6d3d95e1a31ec717ecef4d
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-06 12:33:45 +0700

    set up express session and passport config

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/User');
 
 mongoose.connect('mongodb://localhost/react_fullstack_timetable');
 
+
+//Session Config
+app.use(require('express-session')({
+  secret: "cat session",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Passport Config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Routes
 app.get('/',(req,res) => {
   return res.send('Hello backend');
 });
+
 
 app.listen(3000);
