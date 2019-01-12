8a5563dd2cb2864ff7ff3b64c2e509f4dcb254a7
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-12 22:42:00 +0700

    add days checkbox in course form and add days in courses model

--- client/src/components/CourseForm.js
+++ client/src/components/CourseForm.js
@@ -1,38 +1,51 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-const CourseForm = () => {
-  return (
-    <form action="/courses" method="POST" className="ui form">
-
-      <div className="field">
-        <label htmlFor="name">Course Name</label>
-        <input name="name" type="text" />
+class CourseForm extends Component {
+  renderCheckbox(day){
+    return(
+      <div className="ui checkbox">
+        <input name={day} type="checkbox"/>
+        <label>{day}</label>
       </div>
+    );
+  }
 
-      <div className="field">
-        <label>Duration</label>
+  render(){
+    const days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+    return (
+      <form action="/courses" method="POST" className="ui form">
+
+        <div className="field">
+          <label htmlFor="name">Course Name</label>
+          <input name="name" type="text" />
+        </div>
+
+        <div className="field">
+          <label>Duration</label>
           <div className="inline field">
             <input name="timeStart" type="time" />
             <input name="timeEnd" type="time" />
           </div>
-          <div className="inline field">
+        </div>
 
-          </div>
-      </div>
+        <div className="field">
+          {days.map(day => <div className="inline field">{this.renderCheckbox(day)}</div>)}
+        </div>
 
-      <div className="field">
-        <label htmlFor="place">Place</label>
-        <input name="place" type="text" />
-      </div>
+        <div className="field">
+          <label htmlFor="place">Place</label>
+          <input name="place" type="text" />
+        </div>
 
-      <div className="field">
-        <label htmlFor="instructor">Instructor</label>
-        <input name="instructor" type="text" />
-      </div>
+        <div className="field">
+          <label htmlFor="instructor">Instructor</label>
+          <input name="instructor" type="text" />
+        </div>
 
-      <button>Submit</button>
-    </form>
-  );
+        <button className="ui button">Submit</button>
+      </form>
+    );
+  }
 }
 
 export default CourseForm;
