1b69a6916f1f596be7b458f31d7332537dff2747
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-14 19:40:08 +0700

    change days array to global and sort all days array

--- client/src/components/Timetable.js
+++ client/src/components/Timetable.js
@@ -4,41 +4,51 @@ import { Link } from 'react-router-dom';
 import { fetchCourses } from '../actions';
 
 class Timetable extends Component {
+   sunday = []
+   monday = []
+   tueday = []
+   wednesday = []
+   thursday = []
+   friday = []
+   saturday = []
+
   componentDidMount(){
     this.props.fetchCourses();
   }
 
   classifyDay(){
-    const sunday = [];
-    const monday = [];
-    const tueday = [];
-    const wednesday = [];
-    const thursday = [];
-    const friday = [];
-    const saturday = [];
     this.props.courses.forEach(course => {
       course.days.forEach(day => {
         switch(day){
           case 'sunday':
-            sunday.push(course);break;
+            this.sunday.push(course);break;
           case 'monday':
-            monday.push(course);break;
+            this.monday.push(course);break;
           case 'tueday':
-            tueday.push(course);break;
+            this.tueday.push(course);break;
           case 'wednesday':
-            wednesday.push(course);break;
+            this.wednesday.push(course);break;
           case 'thursday':
-            thursday.push(course);break;
+            this.thursday.push(course);break;
           case 'friday':
-            friday.push(course);break;
+            this.friday.push(course);break;
           case 'saturday':
-            saturday.push(course);break;
+            this.saturday.push(course);break;
           default:
             break;
         }
       });
   });
-  monday.sort((a,b) => parseInt(b.timeStart) - parseInt(a.timeStart));
+  
+  const sortFn=(a,b) => parseInt(b.timeStart) - parseInt(a.timeStart);
+  this.sunday.sort(sortFn);
+  this.monday.sort(sortFn);
+  this.tueday.sort(sortFn);
+  this.wednesday.sort(sortFn);
+  this.thursday.sort(sortFn);
+  this.friday.sort(sortFn);
+  this.saturday.sort(sortFn);
+
   // console.log(monday);
 }
   render(){
