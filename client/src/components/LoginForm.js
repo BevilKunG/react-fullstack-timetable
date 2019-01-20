160700f1a9ca0dff3090e5eab8a9c9f2eedbdc16
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-20 14:41:47 +0700

    do not show login page when already login and rename react web name

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
 
-const LoginForm = () => {
-  return (
-    <form action="/login" method="POST" className="ui form">
-      <div className="field">
-        <label htmlFor="username">Username</label>
-        <input name="username" type="text"/>
-      </div>
+class LoginForm extends Component {
+  checkUserRenderPage(){
+    switch(this.props.auth){
+      case null:
+        return;
+      case '':
+        return (
+          <form action="/login" method="POST" className="ui form">
+            <div className="field">
+              <label htmlFor="username">Username</label>
+              <input name="username" type="text"/>
+            </div>
 
-      <div className="field">
-        <label htmlFor="password">Password</label>
-        <input name="password" type="password"/>
-      </div>
+            <div className="field">
+              <label htmlFor="password">Password</label>
+              <input name="password" type="password"/>
+            </div>
 
-      <button className="ui button">Sign In</button>
-    </form>
-  );
+            <button className="ui button">Sign In</button>
+          </form>
+        );
+      default:
+        return;
+    }
+  }
+
+  render(){
+    return (
+      <>
+        {this.checkUserRenderPage()}
+      </>
+    )
+  }
 }
 
-export default LoginForm;
+const mapStateToProps = ({ auth }) => {
+  return {auth};
+}
+
+export default connect(mapStateToProps)(LoginForm);
