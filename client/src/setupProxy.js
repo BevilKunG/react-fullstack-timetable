9ccf4070f2a2b2fb3e2db55709a0fed3e19612f5
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-01-20 13:18:51 +0700

    remove course border and comment row divided line

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,11 @@
-// const proxy = require('http-proxy-middleware');
-//
-// // Use on Dev Mode only
-//
-// module.exports = function(app){
-//   app.use(proxy('/register',{target: 'http://localhost:5000'}));
-//   app.use(proxy('/login',{target: 'http://localhost:5000'}));
-//   app.use(proxy('/logout',{target: 'http://localhost:5000'}));
-//   app.use(proxy('/api/*',{target: 'http://localhost:5000'}));
-//   app.use(proxy('/courses',{target: 'http://localhost:5000'}));
-// }
+const proxy = require('http-proxy-middleware');
+
+// Use on Dev Mode only
+
+module.exports = function(app){
+  app.use(proxy('/register',{target: 'http://localhost:5000'}));
+  app.use(proxy('/login',{target: 'http://localhost:5000'}));
+  app.use(proxy('/logout',{target: 'http://localhost:5000'}));
+  app.use(proxy('/api/*',{target: 'http://localhost:5000'}));
+  app.use(proxy('/courses',{target: 'http://localhost:5000'}));
+}
